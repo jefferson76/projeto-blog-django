3ff12be3aefdebb5a3b8e675461263aa3bc83d8e
jefferson pereira <jefferson.pereira76@gmail.com>
2023-06-13 14:06:25 -0300

    Criando o model SiteSetup e registrando na admin do Django

--- djangoapp/site_setup/migrations/0001_initial.py
+++ djangoapp/site_setup/migrations/0001_initial.py
@@ -14,7 +14,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MenuLink',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.CharField(max_length=50)),
                 ('url_or_path', models.CharField(max_length=2048)),
                 ('new_tab', models.BooleanField(default=False)),
