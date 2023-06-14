b1a2fd339815007618f1fc981f10b792d8829365
jefferson pereira <jefferson.pereira76@gmail.com>
2023-06-14 12:12:32 -0300

    Criando o model Post, related_name em ForeignKey

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -7,12 +7,13 @@ class MenuLink(models.Model):
     class Meta:
         verbose_name = 'Menu Link'
         verbose_name_plural = 'Menu Links'
+
     text = models.CharField(max_length=50)
     url_or_path = models.CharField(max_length=2048)
     new_tab = models.BooleanField(default=False)
     site_setup = models.ForeignKey(
         'SiteSetup', on_delete=models.CASCADE, blank=True, null=True,
-        default=None,
+        default=None, related_name='menu'
     )
 
     def __str__(self):
